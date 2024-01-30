c6452b86a85185966e8066c39d5d7b0ff71d150d
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-01-30 20:24:46 +0700

    challenge 1/366

---
+++ 1-grow.js
@@ -0,0 +1,9 @@
+function grow(x){
+    let result = x[0];
+    for(let i = 1; i < x.length; i++) {
+        result = result * x[i];
+    }
+    return result;
+}
+
+console.log(grow([2, 2, 2, 2, 2, 2]))
