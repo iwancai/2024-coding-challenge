bc89bd6f04ea0b0e46f3890a2972a699110f1d0f
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-01-30 20:40:29 +0700

    challenge 3/366

---
+++ 3-hero.js
@@ -0,0 +1,9 @@
+function hero(bullets, dragons){
+    if (bullets / dragons > 2) {
+        return true
+    } else false
+}
+
+console.log(hero(7, 4))
+
+// challenge link: https://www.codewars.com/kata/59ca8246d751df55cc00014c/javascript
