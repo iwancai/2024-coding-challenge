4c53173371d2b08bb566c5f021fc88b7fc450d40
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-01-30 20:58:48 +0700

    challenge 4/366

---
+++ 4-smash.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+
+// 4/366
+
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+// function smash (words) {
+//     return words.join(" ")
+//  };
+
+const smash = (words) => words.join(' ')
+
+ console.log(smash(["this", "is", "a", "really", "long", "sentence"]))
