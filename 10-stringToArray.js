5b086f8bd3b9fd06235eb3a7b2b43e251a14f849
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-02-08 07:19:12 +0700

    challenge 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+
+// 10/366
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/
+
+
+function stringToArray(string){
+  return string.split(' ')
+}
+
+
+console.log(stringToArray("I love arrays they are my favorite"))
