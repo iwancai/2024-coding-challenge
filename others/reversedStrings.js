6f076ec1621befabd0ff49a7077e204939923a52
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-02-11 15:23:53 +0700

    others  cdw 4

---
+++ others/reversedStrings.js
@@ -0,0 +1,11 @@
+
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/
+
+// function reverseStrings(str){
+//     return str.split("").reverse().join('')
+// }
+
+const reverseStrings = (str) => str.split("").reverse().join('')
+
+console.log(reverseStrings('world'));
+console.log(reverseStrings('hello'));
