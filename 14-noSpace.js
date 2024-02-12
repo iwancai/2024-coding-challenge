306ad7c87b353e8380cf7529622191519f62651b
Muhamad Ridwan <ridwan.cai@gmail.com>
2024-02-12 20:16:28 +0700

    challenge 14/366

---
+++ 14-noSpace.js
@@ -0,0 +1,14 @@
+// WPU Coding Challenge 2024
+
+// 4/366
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/
+
+
+    function noSpace(x){
+        return x.replace(/\s/g, '')
+    }
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+console.log(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')); 
+console.log(noSpace('8aaaaa dddd r     ')); 
